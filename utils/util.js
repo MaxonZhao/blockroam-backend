9be99e2ac2fb1a4a6c56b5604ba67e93094b3da0
MaxonZhao <maxonzhao@gmail.com>
2022-06-10 15:46:49 -0700

    feat: implement utility methods

---
+++ utils/util.js
@@ -0,0 +1,16 @@
+class RandomUtils {
+    constructor() {}
+    static getRandomInt(max) {
+        return Math.floor(Math.random() * max)
+    }
+    
+    static getRandomFloat(max, decimals) {
+        return (Math.random() * max).toFixed(decimals)
+    }
+
+    static getRandomDate(start, end) {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    }
+}
+
+module.exports = {RandomUtils}
